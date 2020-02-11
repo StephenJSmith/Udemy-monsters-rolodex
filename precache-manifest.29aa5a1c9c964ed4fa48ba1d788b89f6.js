620f07b19dbf0fa4ec799c20d456c62d56f9abfe
Stephen Smith <stephensmith_aus@yahoo.com.au>
2020-02-12 07:42:20 +1100

    Updates

---
+++ precache-manifest.29aa5a1c9c964ed4fa48ba1d788b89f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ca9cb7327ff2e45f1efc73823aeeeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b1f83cc5af39ece0fbd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "83164e851223ad02152f",
+    "url": "/static/js/2.93a320e1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.93a320e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1f83cc5af39ece0fbd",
+    "url": "/static/js/main.7138e325.chunk.js"
+  },
+  {
+    "revision": "9111f876851a4b4d8ecb",
+    "url": "/static/js/runtime-main.56eb31af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
